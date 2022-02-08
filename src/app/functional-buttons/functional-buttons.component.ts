92f90687572b9b085d3e057a966132bcf2fe0dcd
sterownik <4golda4@gmail.com>
2022-02-08 21:34:03 +0100

    feat(app-engine): handing actions buttons
    In this commit i create function which use to handling actions buttons

--- src/app/functional-buttons/functional-buttons.component.ts
+++ src/app/functional-buttons/functional-buttons.component.ts
@@ -1,11 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { HandlingButtons } from '../defs/handball-web.defs';
 
 @Component({
   selector: 'app-functional-buttons',
   templateUrl: './functional-buttons.component.html',
 })
 export class FunctionalButtonsComponent implements OnInit {
+  @Output() handlingButtons = new EventEmitter<HandlingButtons>();
   constructor() {}
 
   ngOnInit(): void {}
+
+  handlingButtonsEmmiter(type: HandlingButtons): void {
+    this.handlingButtons.emit(type);
+  }
 }
--- src/app/game-view/game-view.component.ts
+++ src/app/game-view/game-view.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
-import { SingleQuestion, Counter } from '../defs/handball-web.defs';
-import * as QuestionsJson from '../../assets/questions/questions.json'
+import { SingleQuestion, Counter, HandlingButtons } from '../defs/handball-web.defs';
+import * as QuestionsJson from '../../assets/questions/questions.json';
 
 @Component({
   selector: 'app-game-view',
@@ -10,6 +10,7 @@ import * as QuestionsJson from '../../assets/questions/questions.json'
 export class GameViewComponent {
   questions: SingleQuestion[];
   actualQuestion: SingleQuestion;
+  allQuestionNumber: number;
 
   actualNumberQuestion: number;
 
@@ -23,5 +24,38 @@ export class GameViewComponent {
     this.questions = QuestionsJson as SingleQuestion[];
 
     this.actualQuestion = this.questions[this.actualNumberQuestion];
+    this.allQuestionNumber = this.questions.length - 1;
+  }
+
+  handlingButtons(type: HandlingButtons): void {
+    switch (type) {
+      case 'up':
+        this.actualNumberQuestion++;
+        break;
+      case 'down':
+        this.actualNumberQuestion--;
+        break;
+      case 'draw':
+        this.actualNumberQuestion = this.drawNumberQuestion();
+        break;
+      default:
+    }
+
+    this.actualNumberQuestion = this.validNumberQuestion(this.actualNumberQuestion);
+    this.actualQuestion = this.questions[this.actualNumberQuestion];
+  }
+
+  validNumberQuestion(numberQuestion: number): number {
+    if (numberQuestion > this.allQuestionNumber) {
+      return 0;
+    }
+    if (numberQuestion < 0) {
+      return this.allQuestionNumber;
+    }
+    return numberQuestion;
+  }
+
+  drawNumberQuestion(): number {
+    return Math.floor(Math.random() * this.allQuestionNumber) + 0;
   }
 }
