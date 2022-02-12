2d99ce96ae21436ba4cd6eea8f635678cbecec0f
sterownik <4golda4@gmail.com>
2022-02-12 01:13:54 +0100

    feat(app-engine): improve directing to views
    In this commit i improve directing to views

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GameViewComponent } from './game-view/game-view.component';
 import { MainViewComponent } from './main-view/main-view.component';
 
 const routes: Routes = [
-  { path: '', component: MainViewComponent}
+  { path: '', component: MainViewComponent },
+  { path: 'game-view', component: GameViewComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/game-view/game-view.component.ts
+++ src/app/game-view/game-view.component.ts
@@ -5,19 +5,22 @@ import {
   Counter,
   HandlingButtons,
   AnswerMarked,
+  TypeGame,
 } from '../defs/handball-web.defs';
 import * as QuestionsJson from '../../assets/questions/questions.json';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-game-view',
   templateUrl: './game-view.component.html',
 })
-export class GameViewComponent {
+export class GameViewComponent implements OnInit {
   questions: SingleQuestion[];
   actualQuestion: SingleQuestion;
   allQuestionNumber: number;
   passValidQuestions: AnswerMarked[];
+  gameMode: TypeGame;
 
   actualNumberQuestion: number;
 
@@ -25,10 +28,36 @@ export class GameViewComponent {
 
   formGroup: FormGroup;
 
-  constructor(private fb: FormBuilder, private _snackBar: MatSnackBar) {
+  constructor(
+    private fb: FormBuilder,
+    private _snackBar: MatSnackBar,
+    private router: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.router.params.subscribe((params) => {
+      this.gameMode = params['name'];
+    });
+
+    switch (this.gameMode) {
+      case 'main':
+        this.questions = QuestionsJson as SingleQuestion[];
+        break;
+      case 'chosenAnswers':
+        this.questions = JSON.parse(localStorage.getItem('answers') as string);
+        break;
+    }
+
+    if (this.questions.length === 0) {
+      return;
+    }
+
+    this.prepareQuestion();
+  }
+
+  prepareQuestion(): void {
     this.actualNumberQuestion = 0;
-    this.formGroup = fb.group({});
-    this.questions = QuestionsJson as SingleQuestion[];
+    this.formGroup = this.fb.group({});
 
     this.actualQuestion = this.questions[this.actualNumberQuestion];
     this.allQuestionNumber = this.questions.length - 1;
@@ -48,6 +77,7 @@ export class GameViewComponent {
         break;
       default:
     }
+    this.passValidQuestions = [];
 
     this.actualNumberQuestion = this.validNumberQuestion(
       this.actualNumberQuestion
@@ -81,24 +111,84 @@ export class GameViewComponent {
         )
       )
     ) {
-      this._snackBar.open('Dobra odpowiedź', undefined, {
-        duration: 3000,
-        panelClass: 'info-snackbar',
-      });
-      this.passValidQuestions = [];
-      return this.handlingButtons('up');
+      return this.inCaseValidAnswer();
     }
 
+    this.inCaseInValidAnswer();
+  }
+
+  inCaseValidAnswer(): void {
+    this._snackBar.open('Dobra odpowiedź', undefined, {
+      duration: 3000,
+      panelClass: 'info-snackbar',
+    });
+    this.passValidQuestions = [];
+    return this.handlingButtons('up');
+  }
+
+  inCaseInValidAnswer() {
     this._snackBar.open('Zła odpowiedź!', undefined, {
       duration: 3000,
       panelClass: 'alert-snackbar',
     });
 
     this.passValidQuestions = this.actualQuestion.correctAnswers;
+
+    const that = this;
+    setTimeout(() => {
+      that.passValidQuestions = [];
+    }, 5000);
   }
 
   parseAnswersToArray(value: any): AnswerMarked[] {
     const keys = Object.keys(value);
     return keys.filter((key) => value[key]) as AnswerMarked[];
+  }
+
+  popQuestion(): void {
+    let array = this.getAndSaveArray();
+    console.log(array);
+    console.log(this.actualNumberQuestion);
+    const index = array.findIndex((question: SingleQuestion) =>
+      question.question.match(this.actualQuestion.question)
+    );
+    array.splice(index, 1);
+    localStorage.setItem('answers', JSON.stringify(array));
+    this.questions = array;
+    if (this.actualNumberQuestion >= array.length) {
+      this.actualNumberQuestion--;
+    }
+    this.allQuestionNumber = array.length - 1;
+
+    this.actualQuestion = array[this.actualNumberQuestion];
+    this.showInformation('Usunąłeś pytanie');
+    this.passValidQuestions = [];
+  }
+
+  getAndSaveArray(): SingleQuestion[] {
+    let array = [];
+    let items = JSON.parse(localStorage.getItem('answers') as string) || [];
+    array = items;
+    return array;
+  }
+
+  pushQuestion(): void {
+    let array = this.getAndSaveArray();
+    if (
+      array.some((question: SingleQuestion) =>
+        question.question.match(this.actualQuestion.question)
+      )
+    )
+      return;
+    array.push(this.actualQuestion);
+    localStorage.setItem('answers', JSON.stringify(array));
+    this.showInformation('Dodałeś pytanie!');
+  }
+
+  showInformation(text: string): void {
+    this._snackBar.open(text, undefined, {
+      duration: 3000,
+      panelClass: 'warining-snackbar',
+    });
   }
 }
